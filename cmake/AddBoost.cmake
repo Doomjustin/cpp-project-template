fcc45e81d036d158d3a83d22c679101c1f86c3cb
doom <1075101233doom@gmail.com>
2022-08-20 22:46:19 +0900

    [fix]修改git仓库地址为Https

--- cmake/AddBoost.cmake
+++ cmake/AddBoost.cmake
@@ -7,7 +7,7 @@ endif()
 
 FetchContent_Declare(
   boost
-  GIT_REPOSITORY    git@github.com:boostorg/boost.git # 指定git仓库地址
+  GIT_REPOSITORY    https://github.com/boostorg/boost.git # 指定git仓库地址
   GIT_TAG           boost-1.79.0 # 指定版本
   SOURCE_DIR        ${CMAKE_SOURCE_DIR}/third-party/boost   # 源码下载到此目录下
 )
--- cmake/AddGlfw.cmake
+++ cmake/AddGlfw.cmake
@@ -7,9 +7,9 @@ endif()
 
 FetchContent_Declare(
   glfw
-  GIT_REPOSITORY    git@github.com:glfw/glfw.git # 指定git仓库地址
+  GIT_REPOSITORY    https://github.com/glfw/glfw.git # 指定git仓库地址
   GIT_TAG           3.3-stable # 指定版本
-  SOURCE_DIR        ${CMAKE_SOURCE_DIR}/third-party/boost   # 源码下载到此目录下
+  SOURCE_DIR        ${CMAKE_SOURCE_DIR}/third-party/glfw   # 源码下载到此目录下
 )
 
 FetchContent_MakeAvailable(glfw)
