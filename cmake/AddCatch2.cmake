e7c44e42f0aff6e97edee024840ee65455574379
doom <1075101233doom@gmail.com>
2024-01-03 18:47:25 +0900

    [update]up version

--- cmake/AddCatch2.cmake
+++ cmake/AddCatch2.cmake
@@ -1,13 +1,13 @@
 include(FetchContent)
 
 FetchContent_Declare(
-  Catch2
+  catch2
   GIT_REPOSITORY    https://github.com/catchorg/Catch2.git # 指定git仓库地址
   GIT_TAG           v3.5.1 # 指定版本
-  SOURCE_DIR        ${CMAKE_SOURCE_DIR}/third-party/Catch2   # 源码下载到此目录下
+  SOURCE_DIR        ${CMAKE_SOURCE_DIR}/external/catch2   # 源码下载到此目录下
 )
 
-FetchContent_MakeAvailable(Catch2)
+FetchContent_MakeAvailable(catch2)
 
 list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/extras)
 include(Catch)
--- cmake/AddFmt.cmake
+++ cmake/AddFmt.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   fmt
   GIT_REPOSITORY    https://github.com/fmtlib/fmt.git # 指定git仓库地址
-  GIT_TAG           8.1.1 # 指定版本
+  GIT_TAG           10.2.0 # 指定版本
   SOURCE_DIR        ${CMAKE_SOURCE_DIR}/third-party/fmt   # 源码下载到此目录下
 )
 
