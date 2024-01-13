699ec6d494390ecc0bf076e5465d65efd88f56ef
doom <1075101233doom@gmail.com>
2024-01-14 00:56:06 +0900

    [fix]fix submodule add bug

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ A template project for C++ project
 根据需要自行删除或增加CMakeLists.txt中的add_submodule函数。<br/>
 It might spend a long time when you first time to build，cause downloading third party repositories。<br/>
 Feel free to use cmake function under cmake/ directory, or to modify by your self.
+
+## Usage
+git clone --re
--- cmake/Submodule.cmake
+++ cmake/Submodule.cmake
@@ -1,64 +1,66 @@
-find_package(Git REQUIRED)
-include(CMakePrintHelpers)
-
-cmake_print_variables(GIT_VERSION_STRING)
-
-function(remove_submodule NAME)
-    message(STATUS "Executing: git rm -f extern/${NAME}")
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rm -f extern/${NAME}
-        WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
-        ERROR_QUIET
-    )
-
-    message(STATUS "Executing: rm -rf .git/modules/extern/${NAME}")
-    execute_process(
-        COMMAND rm -rf ./git/modules/extern/${NAME}
-        WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
-        ERROR_QUIET
-    )
-
-    message(STATUS "Executing: git config --remove-section submodule.extern/${NAME}")
-    execute_process(
-        COMMAND git config --remove-section submodule.extern/${NAME}
-        WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
-        ERROR_QUIET
-    )
-endfunction()
-
-function(add_submodule NAME REP_URL)
-    if (NOT EXISTS "${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME}/.git")
-        message(STATUS "Executing: git submodule add ${REP_URL} extern/${NAME}")
-        execute_process(
-            COMMAND ${GIT_EXECUTABLE} submodule add ${REP_URL} extern/${NAME}
-            WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
-            RESULT_VARIABLE RESULT
-        )
-
-        if (NOT RESULT EQUAL "0")
-            message(FATAL_ERROR "git submodule add ${REP_URL} extern/${NAME} failed with ${RESULT}")
-            remove_submodule(${NAME})
-        endif()
-    endif ()
-
-    if (ARGV2)
-        message(STATUS "${NAME}: git checkout ${TAG}")
-        # TODO: 支持切换tag，但是对submodule来说似乎不是很友好，还是指定branch可能会更好
+#function(remove_submodule NAME)
+#    message(STATUS "Executing: git rm -f extern/${NAME}")
+#    execute_process(
+#        COMMAND ${GIT_EXECUTABLE} rm -f extern/${NAME}
+#        WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
+#        ERROR_QUIET
+#    )
+#
+#    message(STATUS "Executing: rm -rf .git/modules/extern/${NAME}")
+#    file(REMOVE_RECURSE ${${PROJECT_NAME}_SOURCE_DIR}/.git/modules/extern/${NAME})
+#
+#    message(STATUS "Executing: git config --remove-section submodule.extern/${NAME}")
+#    execute_process(
+#        COMMAND git config --remove-section submodule.extern/${NAME}
+#        WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
+#        ERROR_QUIET
+#    )
+#endfunction()
+#
+## 似乎是一个没什么用的function
+#function(add_submodule NAME REP_URL)
+#    if (NOT EXISTS "${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME}")
+#        message(STATUS "Executing: git submodule add ${REP_URL} extern/${NAME}")
+#
+#        # 这时候是一个无效rep，删除旧的directory，重新download
+##        if (EXISTS ${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME})
+##            file(REMOVE_RECURSE ${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME})
+##        endif ()
+#
 #        execute_process(
-#            COMMAND git checkout ${ARGV2}
-#            WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME}
+#            COMMAND ${GIT_EXECUTABLE} submodule add ${REP_URL} extern/${NAME}
+#            WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
 #            RESULT_VARIABLE RESULT
 #        )
 #
 #        if (NOT RESULT EQUAL "0")
-#            message(FATAL_ERROR "git checkout ${TAG} failed with ${RESULT}")
+#            message(WARNING "git submodule add ${REP_URL} extern/${NAME} failed with ${RESULT}")
+#            remove_submodule(${NAME})
 #        endif()
-    endif ()
-
-    add_subdirectory("${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME}")
-endfunction()
+#    endif ()
+#
+#    if (ARGV2)
+#        message(STATUS "${NAME}: git checkout ${TAG}")
+#        # TODO: 支持切换tag，但是对submodule来说似乎不是很友好，还是指定branch可能会更好
+##        execute_process(
+##            COMMAND git checkout ${ARGV2}
+##            WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME}
+##            RESULT_VARIABLE RESULT
+##        )
+##
+##        if (NOT RESULT EQUAL "0")
+##            message(FATAL_ERROR "git checkout ${TAG} failed with ${RESULT}")
+##        endif()
+#    endif ()
+#
+#    add_subdirectory("${${PROJECT_NAME}_SOURCE_DIR}/extern/${NAME}")
+#endfunction()
 
 function(update_submodule)
+    find_package(Git REQUIRED)
+    include(CMakePrintHelpers)
+    cmake_print_variables(GIT_VERSION_STRING)
+
     message(STATUS "Executing: git submodule update --init --recursive")
     execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
             WORKING_DIRECTORY ${${PROJECT_NAME}_SOURCE_DIR}
